50fb8063a7dc31dcb634eadb61020560fc1d10ea
Mujtaba <72826134+mujtabafromthedarkside@users.noreply.github.com>
2023-06-28 22:35:56 +0500

    Basic Routing all set up
    in react-router-dom v6, Switch has been replaced with Routes.
    
    component must be called within 'element' property. Otherwise, gives error.
    
    <ul> has some CSS properties that make it horizontal

--- RVTweb/src/App.jsx
+++ RVTweb/src/App.jsx
@@ -1,36 +1,32 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Login from './pages/Login.jsx'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold underline text-center text-green-300">Hello world!</h1> 
-    // <>
-    //   <div>
-    //     <a href="https://vitejs.dev" target="_blank">
-    //       <img src={viteLogo} className="logo" alt="Vite logo" />
-    //     </a>
-    //     <a href="https://react.dev" target="_blank">
-    //       <img src={reactLogo} className="logo react" alt="React logo" />
-    //     </a>
-    //   </div>
-    //   <h1>Vite + React</h1>
-    //   <div className="card">
-    //     <button onClick={() => setCount((count) => count + 1)}>
-    //       count is {count}
-    //     </button>
-    //     <p>
-    //       Edit <code>src/App.jsx</code> and save to test HMR
-    //     </p>
-    //   </div>
-    //   <p className="read-the-docs">
-    //     Click on the Vite and React logos to learn more
-    //   </p>
-    // </>
-  )
+    <Router>
+      <nav>
+        <ul className="flex space-x-4 bg-green-300">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes> 
+        <Route path="/" element={<h1 className="text-3xl font-bold underline text-center text-green-300">Hello world!</h1>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
